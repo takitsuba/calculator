f9cd5ba18bfd60429e11c11dd2c9247dad507979
Hiroo Takizawa <kei.cataract@gmail.com>
2018-03-10 22:03:52 +0900

    add mul func test

--- src/test/scala/CalcSpec.scala
+++ src/test/scala/CalcSpec.scala
@@ -14,4 +14,17 @@ class CalcSpec extends FlatSpec with DiagrammedAssertions {
   it should "Intの最大を上まった際にはオーバーフローする" in {
     assert(calc.sum(Seq(Integer.MAX_VALUE, 1)) === Integer.MIN_VALUE)
   }
+
+  "mul関数" should "整数の配列を取得し、それらを掛け合わせた整数を返すことができる" in {
+    assert(calc.mul(Seq(2,3,4)) === 24)
+    assert(calc.mul(Seq(2,3,0)) === 0)
+    assert(calc.mul(Seq(0)) === 0)
+    assert(calc.mul(Seq(-1,1)) === -1)
+    assert(calc.sum(Seq(-2,-3)) === 6)
+    assert(calc.sum(Seq()) === 0)
+  }
+
+  it should "Intの最大を上まった際にはオーバーフローする" in {
+    assert(calc.mul(Seq(Integer.MAX_VALUE, 2)) === Integer.MIN_VALUE)
+  }
 }
